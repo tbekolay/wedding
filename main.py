9d87818dbe979bd6e30b811bb48ae22be14d17bf
Trevor Bekolay <tbekolay@gmail.com>
2015-01-15 06:58:55 -0500

    Throw up a placeholder
    In case people get their invites within the day.
    Very unlikely, but just in case!

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-from flask import Flask, flash, redirect, render_template
+from flask import Flask, flash, g, redirect, render_template
 
-from forms import LoginForm
+# from forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals.update(zip=zip)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+    # form = RSVPForm()
+    # if form.validate_on_submit():
+    #     flash('Login requested for OpenID="%s", remember_me=%s' %
+    #           (form.openid.data, str(form.remember_me.data)))
+    #     return redirect('/index')
+    # return render_template('index.html', just_submitted=False)
+    return render_template('placeholder.html')
 
 
 @app.errorhandler(404)
